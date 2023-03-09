305dff0cd3bf13241f9d2b782be8c158f466f126
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-03-09 21:43:58 +0700

    chore: main module
    clean up code and make it readable

--- main.py
+++ main.py
@@ -2,22 +2,22 @@
 # License: MIT License
 # Description: A simple calculator app with Python.
 import tkinter as tk
-from logic import create_calculator_gui
-
-# Configure the window
-title = "Calculator"
-size = "474x454"
-font_input = ("Arial", 30, 'bold')
-font_button = ("Arial", 20)
+import logic as calc
 
 # Create a window
-window =  tk.Tk()
+window = tk.Tk()
 
-# Declare Variable
-tkvar = tk.StringVar(window)
+# Configure the window
+CONFIG = {
+    'title': 'Calculator',
+    'size': (410, 500),
+    'font_input': ('Arial', 30, 'bold'),
+    'font_button': ('Arial', 20),
+}
 
-# Create a frame
-create_calculator_gui(tk, window, size, title, tkvar, font_input, font_button)
+# Create the GUI
+calc.Calculator(window, CONFIG['title'], CONFIG['size'], CONFIG['font_input'], CONFIG['font_button'])
 
 # Run the app
-window.mainloop()
+if __name__ == "__main__":
+    window.mainloop()
